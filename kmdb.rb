cd439e34f6d0bb3f5c860c916f228759b478a69c
Ramadityanand <ram.bhogadi@kellogg.northwestern.edu>
2022-02-08 15:21:19 +0000

    data entered in the models

--- kmdb.rb
+++ kmdb.rb
@@ -173,6 +173,99 @@ movie3.save
 
 # Entering data for Roles model
 
+bb = Movie.where({ title: "Batman Begins"})[0]
+dk = Movie.where({ title: "The Dark Knight"})[0]
+dkr = Movie.where({ title: "The Dark Knight Rises"})[0]
+
+role1 = Role.new
+role1.movie_id = bb.id
+role1.person_id = bale.id
+role1.character_name = "Bruce Wayne"
+role1.save
+
+role2 = Role.new
+role2.movie_id = bb.id
+role2.person_id = caine.id
+role2.character_name = "Alfred"
+role2.save
+
+role3 = Role.new
+role3.movie_id = bb.id
+role3.person_id = liam.id
+role3.character_name = "Ra's Al Ghul"
+role3.save
+
+role4 = Role.new
+role4.movie_id = bb.id
+role4.person_id = holmes.id
+role4.character_name = "Rachel Dawes"
+role4.save
+
+role5 = Role.new
+role5.movie_id = bb.id
+role5.person_id = gary.id
+role5.character_name = "Commissioner Gordon"
+role5.save
+
+role6 = Role.new
+role6.movie_id = dk.id
+role6.person_id = bale.id
+role6.character_name = "Bruce Wayne"
+role6.save
+
+role7 = Role.new
+role7.movie_id = dk.id
+role7.person_id = heath.id
+role7.character_name = "Joker"
+role7.save
+
+role8 = Role.new
+role8.movie_id = dk.id
+role8.person_id = aaron.id
+role8.character_name = "Harvey Dent"
+role8.save
+
+role9 = Role.new
+role9.movie_id = dk.id
+role9.person_id = caine.id
+role9.character_name = "Alfred"
+role9.save
+
+role10 = Role.new
+role10.movie_id = dk.id
+role10.person_id = maggie.id
+role10.character_name = "Rachel Dawes"
+role10.save
+
+role11 = Role.new
+role11.movie_id = dkr.id
+role11.person_id = bale.id
+role11.character_name = "Bruce Wayne"
+role11.save
+
+role12 = Role.new
+role12.movie_id = dkr.id
+role12.person_id = gary.id
+role12.character_name = "Commissioner Gordon"
+role12.save
+
+role13 = Role.new
+role13.movie_id = dkr.id
+role13.person_id = tom.id
+role13.character_name = "Bane"
+role13.save
+
+role14 = Role.new
+role14.movie_id = dkr.id
+role14.person_id = joe.id
+role14.character_name = "John Blake"
+role14.save
+
+role15 = Role.new
+role15.movie_id = dkr.id
+role15.person_id = anne.id
+role15.character_name = "Selina Kyle"
+role15.save
 
 # Prints a header for the movies output
 puts "Movies"
